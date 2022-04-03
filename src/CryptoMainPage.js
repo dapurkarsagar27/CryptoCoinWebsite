9541cbde165925f5e20cd8954b07b581fa2b2480
Sagar <sagardapurkar2727@gmail.com>
2022-04-03 23:54:55 +0530

    updated Moralis in main page

--- src/CryptoMainPage.js
+++ src/CryptoMainPage.js
@@ -8,43 +8,49 @@ import ClientSection from "./ClientSection";
 import License from "./License";
 import SignUp from "./SignUp";
 import Footer from "./Footer";
+import { MoralisProvider } from "react-moralis";
 
 const MainApp = () => {
   return (
     <>
-      <div className="main-div">
-        <div className="content-div">
-          <Header />
-          <Content />
-        </div>
+      <MoralisProvider
+        serverUrl="https://z2xzdjtckenv.usemoralis.com:2053/server"
+        appId="0T1zs7PLRUGiKT3jgVL5jtgtEfbyEgg9KLavFaNR"
+      >
+        <div className="main-div">
+          <div className="content-div">
+            <Header />
+            <Content />
+          </div>
 
-        <div className="info-div">
-          <div className="info-div-branch">
-            <InfoContent />
-            <VideoSection />
+          <div className="info-div">
+            <div className="info-div-branch">
+              <InfoContent />
+              <VideoSection />
+            </div>
+          </div>
+
+          <div className="social-div">
+            <div className="social-div-branch">
+              <SocialSection />
+            </div>
+          </div>
+
+          <div className="client-div">
+            <div className="client-div-branch">
+              <ClientSection />
+              <License />
+              <SignUp />
+            </div>
+          </div>
+
+          <div className="footer-div">
+            <div className="footer-div-branch">
+              <Footer />
+            </div>
           </div>
         </div>
-
-        <div className="social-div">
-          <div className="social-div-branch">
-            <SocialSection />
-          </div>
-        </div>
-
-        <div className="client-div">
-          <div className="client-div-branch">
-            <ClientSection />
-            <License />
-            <SignUp />
-          </div>
-        </div>
-
-        <div className="footer-div">
-          <div className="footer-div-branch">
-            <Footer />
-          </div>
-        </div>
-      </div>
+      </MoralisProvider>
     </>
   );
 };
